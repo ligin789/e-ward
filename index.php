664bdd511746703c9db1254344930c2bad301179
Manas P <manas4518pachakkil@gmail.com>
2021-10-29 02:33:38 +0530

    Login Content Section

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Ward | Login</title>
+    <link rel="stylesheet" href="./assets/styles/style.css">
+</head>
+<body>
+    <section class="main">
+        <div class="sections">
+            <div class="content">
+                <div class="logo">
+                    <img src="./assets/images/logo.svg" alt="Logo">
+                </div>
+                <div class="description">
+                    The goal of this web application is to reduce the amount of 
+                    work that ward members have to do, make their jobs easier, 
+                    and make their activities more transparent. Here, manual 
+                    tasks are transformed to digital, allowing paper tasks to 
+                    be reduced to online tasks.
+                </div>
+                <div class="connection">
+                    <img src="./assets/images/connections.png" alt="Connections">
+                </div>
+            </div>
+            <div class="form">wer</div>
+        </div>
+    </section>
+</body>
+</html>
